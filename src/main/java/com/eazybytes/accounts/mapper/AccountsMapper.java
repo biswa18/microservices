eb9c70a0423daa78b7a537e50af43bb52a3afd9f
Biswadeb Mukhopadhyay <mukherjeebiswadeb@gmail.com>
2023-12-24 23:14:51 +0530

    Acc-1.1: Commented the accountsmapper for now.

--- src/main/java/com/eazybytes/accounts/mapper/AccountsMapper.java
+++ src/main/java/com/eazybytes/accounts/mapper/AccountsMapper.java
@@ -4,17 +4,17 @@ import com.eazybytes.accounts.dto.AccountsDto;
 import com.eazybytes.accounts.entity.Accounts;
 
 public class AccountsMapper {
-	public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
-		accountsDto.setAccountNumber(accounts.getAccountNumber());
-		accountsDto.setAccountType(accounts.getAccountType());
-		accountsDto.setBranchAddress(accounts.getBranchAddress());
-		return accountsDto;
-	}
-
-	public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
-		accounts.setAccountNumber(accountsDto.getAccountNumber());
-		accounts.setAccountType(accountsDto.getAccountType());
-		accounts.setBranchAddress(accountsDto.getBranchAddress());
-		return accounts;
-	}
+//	public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
+//		accountsDto.setAccountNumber(accounts.getAccountNumber());
+//		accountsDto.setAccountType(accounts.getAccountType());
+//		accountsDto.setBranchAddress(accounts.getBranchAddress());
+//		return accountsDto;
+//	}
+//
+//	public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
+//		accounts.setAccountNumber(accountsDto.getAccountNumber());
+//		accounts.setAccountType(accountsDto.getAccountType());
+//		accounts.setBranchAddress(accountsDto.getBranchAddress());
+//		return accounts;
+//	}
 }
