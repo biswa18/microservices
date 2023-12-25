6325515ea69bf317dbfb885318350266a602df15
biswa18 <mukherjeebiswadeb@gmail.com>
2023-12-25 22:02:03 +0530

    Acc-1.1: Enhancing Swagger ui doc info OpenApiDocumentation annotation

--- src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -4,8 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST Api documentation",
+				description = "Demo Accounts microservice REST Api documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Biswadeb Mukhopadhyay",
+						email = "mukherjeebiswadeb@gmail.com"
+						)
+				),
+		   externalDocs = @ExternalDocumentation(
+				   url = "https://springdoc.org/",
+				   description = "OpenApi official doc")
+		)
 public class AccountsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AccountsApplication.class, args);
--- src/main/java/com/eazybytes/accounts/service/impl/IAccountServiceImpl.java
+++ src/main/java/com/eazybytes/accounts/service/impl/IAccountServiceImpl.java
@@ -35,8 +35,6 @@ public class IAccountServiceImpl implements IAccountService {
 			throw new CustomerAlreadyExistsException(
 					"Customer already exists with mobile number: " + customerDto.getMobile_number());
 		
-//		customer.setCreated_by("user123");
-//		customer.setCreated_at(LocalDateTime.now());
 		var savedCustomer = customerRepository.save(customer); // this will fetch customer id after successfull save
 		
 		accountsRepository.save(createNewAccount(savedCustomer));
@@ -48,8 +46,6 @@ public class IAccountServiceImpl implements IAccountService {
         newAccount.setCustomerId(customer.getCustomer_id());
         long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
 
-//        newAccount.setCreated_by("user123");
-//        newAccount.setCreated_at(LocalDateTime.now());
         newAccount.setAccount_number(randomAccNumber);
         newAccount.setAccount_type(AccountsConstants.SAVINGS);
         newAccount.setBranch_address(AccountsConstants.ADDRESS);
